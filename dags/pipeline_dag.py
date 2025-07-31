e3201ecf89c8c7fe69486db6dad6dc50fc1c39f9
joaomarcelgomes <joaomarcelogomes7@gmail.com>
2025-07-31 18:19:51 -0300

    ...

--- dags/pipeline_dag.py
+++ dags/pipeline_dag.py
@@ -1,15 +1,19 @@
 from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime
+from pathlib import Path
+from docker.types import Mount as DockerMount
 
 default_args = {
     'start_date': datetime(2025, 7, 1),
 }
 
+CSV_DIR = str(Path(__file__).resolve().parent.parent / 'data/csv')
+
 def create_operator(taskid, image, command):
     operator = DockerOperator(
         task_id=taskid,
-        image=f'dataflow-{image}', 
+        image=f'dataflow_{image}', 
         command=f'python {command}.py',
         docker_url='unix://var/run/docker.sock',
         network_mode='dataflow_airflow_net',
@@ -25,16 +29,17 @@ with DAG(
     schedule_interval=None,
     catchup=False
 ) as dag:
+    run_api_producer = create_operator(
+        'run_api_producer',
+        'api_producer',
+        'fetch_api_producer'
+    )
 
-    # run_db_producer = DockerOperator(
-    #     task_id='run_db_producer',
-    #     image='dataflow-database_producer', 
-    #     command='python extract_database_producer.py',
-    #     docker_url='unix://var/run/docker.sock',
-    #     network_mode='dataflow_airflow_net',
-    #     mount_tmp_dir=False,
-    #     auto_remove=True
-    # )
+    run_api_consumer = create_operator(
+        'run_api_consumer',
+        'api_consumer',
+        'fetch_api_consumer'
+    )
 
     run_db_producer = create_operator(
         'run_db_producer', 
@@ -42,4 +47,24 @@ with DAG(
         'extract_database_producer'
     )
 
-    run_db_producer
+    run_db_consumer = create_operator(
+        'run_db_consumer', 
+        'database_consumer',
+        'database_consumer'
+    )
+
+    run_csv_producer = create_operator(
+        'run_csv_producer',
+        'csv_producer',
+        'csv_reader_producer'
+    )
+
+    run_csv_consumer = create_operator(
+        'run_csv_consumer',
+        'csv_consumer',
+        'csv_reader_consumer'
+    )
+
+    run_api_producer >> run_api_consumer
+    run_db_producer >> run_db_consumer
+    run_csv_producer >> run_csv_consumer
